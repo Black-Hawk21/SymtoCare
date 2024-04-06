30acac9fee085670216c66a558afd0feb101956e
AryanAAB <145079379+AryanAAB@users.noreply.github.com>
2024-04-06 22:06:10 +0530

    Delete pdf_reader.py

--- pdf_reader.py
+++
@@ -1,9 +0,0 @@
-from pypdf import PdfReader 
-  
-# creating a pdf reader object 
-reader = PdfReader('Writing Assignment.pdf')
-
-for i in range(len(reader.pages)):
-    text = reader.pages[i]
-    print(f'######### page-{i + 1} #########')
-    print(text.extract_text())
